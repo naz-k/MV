7aee153f12cd72a11e3a075b80fbea30d705e1ca
farnaz <f.kashandeh@gmail.com>
2021-04-14 14:01:17 -0400

    Associer la commande à l'utilisateur actuel.

--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../auth.service';
 import { CommandeService } from '../commande.service';
 import { Panier } from '../models/panier';
 import { PanierService } from '../panier.service';
@@ -13,24 +14,32 @@ export class VerificationComponent implements OnInit, OnDestroy {
 
   livraison: any = {};
   panier: Panier;
-  subscription: Subscription;
+  idUsager: string;
+  panierSubscription: Subscription;
+  usagerSubscription: Subscription;
+  
 
   constructor(
+    private authService: AuthService,
     private commandeService: CommandeService,
     private panierService: PanierService) {}
 
   async ngOnInit() {
     let panier$ = await this.panierService.recupererPanier();
-    this.subscription = panier$.subscribe(panier => this.panier = panier);
+    this.panierSubscription = panier$.subscribe(panier => this.panier = panier);
+    //uid: est un identifiant unique que Firebase consacre a chaque usager.
+    this.authService.user$.subscribe(usager => this.idUsager = usager.uid)  
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.panierSubscription.unsubscribe();
+    this.usagerSubscription.unsubscribe();
   }
 
   passerCommande() {
     //console.log(this.livraison);
     let commande = {
+      idUsager: this.idUsager,
       dateCommande: new Date().getTime(),
       livraison: this.livraison,
       articles: this.panier.articles.map( a => {
